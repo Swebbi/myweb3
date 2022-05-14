809fddee6c4e6e0526a4b9cda651a9b72c24648e
Swebbi <102279829+Swebbi@users.noreply.github.com>
2022-05-14 00:58:11 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// When clicked on mobile menu button/mobile bar nav bar should appear
+function openMenu(){
+    document.getElementById("nav_bar").style.left="0px";
+    
+}
+
+// When clicked on mobile cross button, nav bar should disappear
+
+function closeMenu(){
+    document.getElementById("nav_bar").style.left="-200px";
+
+}
